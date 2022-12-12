321f6f5286ef1190042ad43cbb9cf61cd60ab3f0
vite1k <viktor16years@icloud.com>
2022-12-12 17:43:26 +0700

    задача 10 решена

---
+++ HW_C#_Seminar2/Task_10/Task_10.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+/*
+int CutNumber(int num)
+{
+    int result = num / 10 % 10;
+
+    return result;
+}
+
+Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int res = CutNumber(a);
+Console.WriteLine(res);
+*/
