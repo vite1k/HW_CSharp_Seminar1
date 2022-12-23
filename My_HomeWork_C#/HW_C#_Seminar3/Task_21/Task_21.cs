4dbbe52e004c90d31f5911ba5df9fd4b20d0aec8
vite1k <viktor16years@icloud.com>
2022-12-23 17:48:56 +0700

    задача 23 решена

---
+++ My_HomeWork_C#/HW_C#_Seminar3/Task_21/Task_21.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+/*
+double DDD(double xA, double yA,double zA, double xB, double yB, double zB)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(xA - xB,2) + Math.Pow(yA - yB,2) + Math.Pow(zA - zB,2)),2);
+}
+Console.Write("Введите данные xA ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите данные yA ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите данные zA ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите данные xB ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите данные yB ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите данные zB ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Distance = {DDD(x1, y1, z1, x2, y2, z2)}");
+*/
