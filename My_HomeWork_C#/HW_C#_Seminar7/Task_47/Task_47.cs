d64b750a1dbd6077fe758f924827811a37fe3bfe
vite1k <viktor16years@icloud.com>
2022-12-23 17:38:38 +0700

    задача 47 решена

---
+++ My_HomeWork_C#/HW_C#_Seminar7/Task_47/Task_47.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+double[,] CreateRandom2dArray(int rows ,int columns, int minValue, int maxValue)
+{
+    double[,] array = new double[rows,columns];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i,j] = Math.Round((new Random().NextDouble() + Convert.ToDouble(new Random().Next(minValue, maxValue))),2);
+        }
+    }
+    return array;
+}
+
+void Show2dArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимально возможное значение: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимально возможное значение: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+double[,] myArray = CreateRandom2dArray(rows, columns, minValue, maxValue);
+Show2dArray(myArray);
