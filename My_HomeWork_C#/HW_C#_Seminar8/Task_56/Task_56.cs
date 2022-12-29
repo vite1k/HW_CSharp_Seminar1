06ffdea42a44170fb97e88b97dc499c2d44b2354
vite1k <viktor16years@icloud.com>
2022-12-29 20:47:22 +0700

    задача 56 решена

---
+++ My_HomeWork_C#/HW_C#_Seminar8/Task_56/Task_56.cs
@@ -0,0 +1,76 @@
+﻿/*
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+int[,] CreateRandom2DArray(int rows ,int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows,columns];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int SumStrArray(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+Console.Clear();
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимально возможное значение: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимально возможное значение: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2DArray(rows, columns, minValue, maxValue);
+
+CreateRandom2DArray(rows ,columns, minValue, maxValue);
+Show2dArray(myArray);
+
+int minSumLine = 0;
+int sumLine = SumStrArray(myArray, 0);
+
+for (int i = 1; i < myArray.GetLength(0); i++)
+{
+  int tempSumLine = SumStrArray(myArray, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой элементов ({sumLine}). ");
